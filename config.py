1c2e0ed3db900f3c79f64a20790ea02e11bd7067
farhan00kham-byte <farhan00kham@gmail.com>
2026-01-12 10:09:12 +0530

    Initialize config.py with API and MongoDB settings
    Added configuration settings for API and database.

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+API_ID = int(os.getenv("31864613"))
+API_HASH = os.getenv("48d0a5b52baef49f1b4c7ab72decb8fa")
+BOT_TOKEN = os.getenv("8593564261:AAGFTklYp-mwuamMknlLRaVU2nC-GZ70h_w")
+
+MONGO_URL = os.getenv("mongodb+srv://Ly_Farhan00:@Farhan@00@cluster0.nxwig31.mongodb.net/?appName=Cluster0")
+
+# Multiple channels force join
+FORCE_SUB_CHANNELS = [
+    "@Anime_Back_0",
+    "@Anime_Back_00",
+    "@Anime_Back_000"
+]
+
+ADMINS = [8438792943]  # अपना Telegram ID डालो
